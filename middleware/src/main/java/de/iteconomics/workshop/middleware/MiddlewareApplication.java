417b243e847c05bfde3e66a04830a27e9ff9e34c
Stefan Schoeffmann <stefan.schoeffmann@posteo.de>
2017-08-27 19:41:28 +0200

    Created a second microservice with camel

---
+++ middleware/src/main/java/de/iteconomics/workshop/middleware/MiddlewareApplication.java
@@ -0,0 +1,12 @@
+package de.iteconomics.workshop.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareApplication.class, args);
+	}
+}
